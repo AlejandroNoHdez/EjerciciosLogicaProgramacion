68147fcd6575ef46a0772baaccaad723fa35eacb
AlejandroNoHdez <nohbryan10@gmail.com>
2023-03-17 19:35:46 -0600

    Ejercicio 2 Resuelto

--- SA-LPD2/Ejercicio-2.js
+++ SA-LPD2/Ejercicio-2.js
@@ -1,3 +1,21 @@
+/*
+
+Bryan Alejandro Noh Hernandez - Ejercicio 2
+
+Crear un programa en Javascript que realice lo siguiente:
+    ○ Debe solicitar al usuario la temperatura en grados Celsius por prompt o por un input.
+    ○ Debe convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
+    ○ Debe imprimir ambos resultados por consola o por el DOM.
+    ○ Debe ser capaz de identificar si los datos de entrada sean de tipo number, en caso contrario debe mandar
+    un mensaje de error y volver a solicitar los datos.
+● Prueba tu programa con las siguientes entradas para asegurarte que funcione correctamente:
+    ○ Entrada:
+        ■ 45.
+    ○ Salida
+        ■ Grados Kelvin: 318.15
+        ■ Grados Fahrenheit: 113.
+*/
+
 function convertTemperature()
 {
     let celsius = parseFloat(prompt("Ingresa la temperatura en grados Celsius:"));
