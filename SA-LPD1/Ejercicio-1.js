710a95a3f23b322db0850adec5492e0ac4112016
AlejandroNoHdez <nohbryan10@gmail.com>
2023-03-17 19:19:49 -0600

    Ejercicio 1 Resuelto

--- SA-LPD1/Ejercicio-1.js
+++ SA-LPD1/Ejercicio-1.js
@@ -29,15 +29,15 @@ let mayor, centro, menor;
 // Identificar si los números son iguales
 if (num1 === num2 && num2 === num3)
 {
-    console.log("Los números son iguales");
+    console.log(`Los números son iguales`);
 }
 else
 {
     // Identificar el número mayor
-    if (num1 > num2 && num1 > num3)
+    if (num1 >= num2 && num1 >= num3)
     {
         mayor = num1;
-        if (num2 > num3)
+        if (num2 >= num3)
         {
             centro = num2;
             menor = num3;
@@ -48,10 +48,10 @@ else
             menor = num2;
         }
     }
-    else if (num2 > num1 && num2 > num3)
+    else if (num2 >= num1 && num2 >= num3)
     {
         mayor = num2;
-        if (num1 > num3)
+        if (num1 >= num3)
         {
             centro = num1;
             menor = num3;
@@ -65,7 +65,7 @@ else
     else
     {
         mayor = num3;
-        if (num1 > num2)
+        if (num1 >= num2)
         {
             centro = num1;
             menor = num2;
